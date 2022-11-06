8f008427935df3aca65b78434de5bc9988a2d47e
Kayky Freitas <kayky_defreitas@hotmail.com>
2022-11-06 03:36:02 -0300

    Correcting redis connection

--- src/shared/infra/http/middleware/rateLimiter.ts
+++ src/shared/infra/http/middleware/rateLimiter.ts
@@ -4,36 +4,31 @@ import * as redis from "redis";
 
 import { AppError } from "@shared/errors/AppError";
 
-const redisClient = redis.createClient({
-  legacyMode: true,
-  socket: {
-    host: process.env.REDIS_HOST,
-    port: Number(process.env.REDIS_PORT),
-    sessionTimeout: 20,
-  },
-});
-
-const limiter = new RateLimiterRedis({
-  storeClient: redisClient,
-  keyPrefix: "rateLimiter",
-  points: 10,
-  duration: 5,
-});
-
 export default async function rateLimiter(
   request: Request,
   response: Response,
   next: NextFunction
 ): Promise<void> {
+  const redisClient = redis.createClient({
+    legacyMode: true,
+    socket: {
+      host: process.env.REDIS_HOST,
+      port: Number(process.env.REDIS_PORT),
+    },
+  });
+
+  await redisClient.connect();
+
+  const limiter = new RateLimiterRedis({
+    storeClient: redisClient,
+    keyPrefix: "rateLimiter",
+    points: 5,
+    duration: 5,
+  });
   try {
-    await redisClient.connect();
-
     await limiter.consume(request.ip);
-
     return next();
-  } catch (e) {
-    throw new AppError("Too many requests", 429);
-  } finally {
-    await redisClient.disconnect();
+  } catch (err) {
+    throw new AppError(err.message, 429);
   }
 }
